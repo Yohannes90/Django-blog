fd0c30c0a4a6bbe2919ec5a95f479a3d84123859
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-02-21 00:44:34 +0300

    updating blog app to fetch from database rather than use dummy data, added posts module and added it to admin

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Mary Jane',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 12, 2024'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 19, 2024'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
