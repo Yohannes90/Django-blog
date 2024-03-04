69ae82829aeb75c06431747e98609bec22484f73
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-05 01:51:52 +0300

    implimenting update user profile and resizing picture

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 
 def register(request):
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Your Account has been successfully created. Welcome { username }, you can now login.")
+            messages.success(request, f"Your Account has been created successfully. Welcome { username }, you can now login.")
             return redirect('login')
     else:
         form = UserRegistrationForm()
@@ -17,4 +17,23 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,
+                                instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Your Account has been updated successfully.")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
