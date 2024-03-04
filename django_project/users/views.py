257ac366b56bf9219845edb7fffc9fcb92e5f413
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-04 20:16:30 +0300

    implementing login and logout system and updating home page accordingly

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 from django.contrib import messages
 
@@ -8,8 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Your Account has been successfully created, Welcome { username }!")
-            return redirect('blog-home')
+            messages.success(request, f"Your Account has been successfully created. Welcome { username }, you can now login.")
+            return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
