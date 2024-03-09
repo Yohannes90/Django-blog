4c13ba4f0cd7f8806e76cfca2af1318754852850
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-03-09 21:24:23 +0300

    updating project file structure

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     # def save(self):
     #     super().save()
     def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
         
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
